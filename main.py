15687ec8a120e1a24629fa489b0dd69ab40320d6
melimat <melimahu@gmail.com>
2019-02-09 20:50:22 +0100

    Added plotting feature, unfortunately so slow, it is to be multithreaded

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 import serial
 import time
 import csv
+import matplotlib.pyplot as plt
+
+plt.ion()
+x_array = []
+y_array = []
+micropipette_amount = 200
 
 port = "COM6"
 baudrate = 9600
 log_csv_path = "log.csv"
 
-titrator = serial.Serial(port, 9600, timeout=1)
-log_file = open(log_csv_path, "a")
+titrator = serial.Serial(port, baudrate, timeout=1)
+log_file = open(log_csv_path, "w", newline="")
 writer = csv.writer(log_file, dialect="excel")
 writer.writerow(["Adds", "Ph"])
 time.sleep(2)
 
 while True:
     ser_bytes = titrator.readline()
-    decoded_bytes = ser_bytes.decode('ascii')
+    decoded_bytes = ser_bytes.decode('utf-8')
+    print(ser_bytes)
     if (len(decoded_bytes) > 1):
         print(decoded_bytes)
-        line_list = decoded_bytes.split(";")
-        number_of_adds_list = line_list[0].split(" ")
-        ph_str_list = line_list[1].split(" ")
-        print("Parsed: Adds: " + number_of_adds_list[1] + " ; Ph: " + ph_str_list[2])
-        adds = int(number_of_adds_list[1])
-        ph = float(ph_str_list[2])
-        writer.writerow([str(adds), str(ph)])
+        line_array = decoded_bytes.split(";")
+        adds_str_array = line_array[0].split(":")
+        ph_str_array = line_array[1].split(":")
+        ph_str_array_second = ph_str_array[1].split("\r\n")
+        print(adds_str_array[1])
+        print(ph_str_array_second[0])
+        adds = int(adds_str_array[1])
+        ph = float(ph_str_array_second[0])
+        x_array.append(adds*micropipette_amount)
+        y_array.append(ph)
+        print(adds)
+        print(ph)
+        writer.writerow([adds, ph])
+        plt.clf()
+        plt.scatter(x_array, y_array)
+        plt.plot(x_array, y_array)
+        plt.draw()
+        plt.pause(1e-17)
+        time.sleep(0.1)
+
 
 
 
