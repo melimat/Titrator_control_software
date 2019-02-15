962fde7a0fcdf1cf4a5b655e7d05059074c19394
melimat <melimahu@gmail.com>
2019-02-15 16:46:12 +0100

    Added arguments to be parsed from teminal for better user expirience

--- SerialPlot.py
+++ SerialPlot.py
@@ -1,4 +1,5 @@
 import serial
+import csv
 from tkinter import *
 import matplotlib.figure as figure
 import matplotlib.animation as animation
@@ -7,69 +8,78 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 com_port = "COM6"
 baudrate = 9600
 timeout = 0
+def serialPlot(com_port, baudrate, timeout, log_path, mp_amount):
+    xar = []
+    yar =[]
+    log_file = open(log_path, mode="w", newline="")
+    log_file_writer = csv.writer(log_file)
+    log_file_writer.writerow(["Added", "Ph"])
+    titrator = serial.Serial(com_port, baudrate, timeout=timeout)
+    def start_function():
+        titrator.write(("START\r\n").encode())
 
-xar = []
-yar =[]
+    def stop_function():
+        titrator.write(("STOP\r\n").encode())
 
-titrator = serial.Serial(com_port, baudrate, timeout=timeout)
-def start_function():
-    titrator.write(("START\r\n").encode())
+    def animate(i, xar, yar):
+        raw_data = titrator.readline()
+        decoded_data = raw_data.decode("utf-8")
+        print(decoded_data)
+        if (len(decoded_data) > 1):
+            line_array = decoded_data.split(";")
+            adds_str_array = line_array[0].split(":")
+            ph_str_array = line_array[1].split(":")
+            ph_str_array_second = ph_str_array[1].split("\r\n")
+            adds = float(adds_str_array[1])
+            ph = float(ph_str_array_second[0])
+            log_file_writer.writerow([adds * mp_amount, ph])
+            print(str(adds) + " - " + str(ph));
+            xar.append(adds * mp_amount)
+            yar.append(ph)
+            ax.clear()
+            ax.plot(xar, yar)
 
-def stop_function():
-    titrator.write(("STOP\r\n").encode())
+    root = Tk()
+    root.title("Titrator controller")
+    frame = Frame(root)
+    frame.configure(bg="white")
+    frame.pack(fill=BOTH, expand=1)
 
-def animate(i, xar, yar):
-    raw_data = titrator.readline()
-    decoded_data = raw_data.decode("utf-8")
-    print(decoded_data)
-    if (len(decoded_data) > 1):
-        line_array = decoded_data.split(";")
-        adds_str_array = line_array[0].split(":")
-        ph_str_array = line_array[1].split(":")
-        ph_str_array_second = ph_str_array[1].split("\r\n")
-        adds = float(adds_str_array[1])
-        ph = float(ph_str_array_second[0])
-        print(str(adds) + " - " + str(ph));
-        xar.append(adds * 200)
-        yar.append(ph)
-        ax.clear()
-        ax.plot(xar, yar)
-
-root = Tk()
-root.title("Titrator controller")
-frame = Frame(root)
-frame.configure(bg="white")
-frame.pack(fill=BOTH, expand=1)
-
-fig = figure.Figure(figsize=(2, 2))
-fig.subplots_adjust(left=0.1, right=0.8)
-ax = fig.add_subplot(1, 1, 1)
+    fig = figure.Figure(figsize=(2, 2))
+    fig.subplots_adjust(left=0.1, right=0.8)
+    ax = fig.add_subplot(1, 1, 1)
 
 
 
-canvas = FigureCanvasTkAgg(fig, master=frame)
-canvas_plot = canvas.get_tk_widget()
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas_plot = canvas.get_tk_widget()
 
-start_button = Button(frame, text="START", command=start_function)
-stop_button = Button(frame, text="STOP", command=stop_function)
+    start_button = Button(frame, text="START", command=start_function)
+    stop_button = Button(frame, text="STOP", command=stop_function)
 
-canvas_plot.grid(row=0,
-                 column=0,
-                 rowspan=5,
-                 columnspan=10,
-                 sticky=W+E+N+S)
+    canvas_plot.grid(row=0,
+                     column=0,
+                     rowspan=5,
+                     columnspan=10,
+                     sticky=W+E+N+S)
 
-start_button.grid(row=5, column=2, columnspan=2)
-stop_button.grid(row=5, column=3, columnspan=2)
+    start_button.grid(row=5, column=2, columnspan=2)
+    stop_button.grid(row=5, column=3, columnspan=2)
 
-for w in frame.winfo_children():
-    w.grid(padx=5, pady=5)
-for i in range(0, 5):
-    frame.rowconfigure(i, weight=1)
-for i in range(0, 5):
-    frame.columnconfigure(i, weight=1)
+    for w in frame.winfo_children():
+        w.grid(padx=5, pady=5)
+    for i in range(0, 5):
+        frame.rowconfigure(i, weight=1)
+    for i in range(0, 5):
+        frame.columnconfigure(i, weight=1)
 
-fargs = (xar, yar)
-ani = animation.FuncAnimation(fig, animate, fargs=fargs, interval=1000)
+    fargs = (xar, yar)
+    ani = animation.FuncAnimation(fig, animate, fargs=fargs, interval=1000)
 
-root.mainloop()
+    def on_closing():
+        titrator.flush()
+        titrator.close()
+        log_file.close()
+        root.destroy()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.mainloop()
--- main.py
+++ main.py
@@ -1,44 +1,17 @@
-import serial
-import time
-import csv
-import matplotlib.pyplot as plt
+import sys
+from SerialPlot import serialPlot
 
 
-x_array = []
-y_array = []
-micropipette_amount = 200
-
-port = "COM6"
-baudrate = 9600
-log_csv_path = "log.csv"
-
-titrator = serial.Serial(port, baudrate, timeout=1)
-log_file = open(log_csv_path, "w", newline="")
-writer = csv.writer(log_file, dialect="excel")
-writer.writerow(["Adds", "Ph"])
-time.sleep(2)
-
-while True:
-    ser_bytes = titrator.readline()
-    decoded_bytes = ser_bytes.decode('utf-8')
-    print(ser_bytes)
-    if (len(decoded_bytes) > 1):
-        print(decoded_bytes)
-        line_array = decoded_bytes.split(";")
-        adds_str_array = line_array[0].split(":")
-        ph_str_array = line_array[1].split(":")
-        ph_str_array_second = ph_str_array[1].split("\r\n")
-        print(adds_str_array[1])
-        print(ph_str_array_second[0])
-        adds = int(adds_str_array[1])
-        ph = float(ph_str_array_second[0])
-        x_array.append(adds*micropipette_amount)
-        y_array.append(ph)
-        print(adds)
-        print(ph)
-        writer.writerow([adds, ph])
-        time.sleep(0.1)
+def main():
+    com_port = str(sys.argv[1])
+    baudrate = int(sys.argv[2])
+    timeout = int(sys.argv[3])
+    log_path = sys.argv[4]
+    mp_amount = int(sys.argv[5])
+    serialPlot(com_port, baudrate, timeout, log_path, mp_amount)
 
 
+if __name__ == '__main__':
+    main()
 
 
